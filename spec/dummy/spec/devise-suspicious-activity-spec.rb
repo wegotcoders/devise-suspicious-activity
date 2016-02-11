ae7966cf8c6807a4098c4f86f1fc21526013d371
Simon Knight <simon@Simons-MacBook-Air.local>
2016-02-11 16:15:05 +0000

    Refactor tests

--- spec/dummy/spec/devise-suspicious-activity-spec.rb
+++ spec/dummy/spec/devise-suspicious-activity-spec.rb
@@ -6,18 +6,23 @@ require 'rails_helper'
 describe "DeviseSuspiciousActivity" do
   before do
     @original_email = 'user@gmail.com'
+
     @user = User.create(email: @original_email, password: 'password')
     @user.email = 'changed@gmail.com'
     @user.save!
+    
+    @mail = ActionMailer::Base.deliveries.last
   end
 
   it "sends one email" do
-    mail = ActionMailer::Base.deliveries.last
     expect(ActionMailer::Base.deliveries.count).to eq 1
-    expect(mail.body.raw_source).to include('We wanted to let you know that your email was changed')
+  end
+
+  it "had the correct email body" do
+    expect(@mail.body.raw_source).to include('We wanted to let you know that your email was changed')
   end
 
   it "sends the correct email to the correct address" do
-    expect(mail.to.first).to eq(@original_email)
+    expect(@mail.to.first).to eq(@original_email)
   end
 end
