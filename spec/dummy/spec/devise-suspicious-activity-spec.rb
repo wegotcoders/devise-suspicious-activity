5236fe32224a0281d6192f28e242db8c2053655d
Simon Knight <simon@Simons-MacBook-Air.local>
2016-02-11 16:07:57 +0000

    Refactored tests

--- spec/dummy/spec/devise-suspicious-activity-spec.rb
+++ spec/dummy/spec/devise-suspicious-activity-spec.rb
@@ -15,12 +15,9 @@ describe "DeviseSuspiciousActivity" do
     mail = ActionMailer::Base.deliveries.last
     expect(ActionMailer::Base.deliveries.count).to eq 1
     expect(mail.body.raw_source).to include('We wanted to let you know that your email was changed')
-    expect(mail.to.first).to eq(@original_email)
-    # Test email body and email sent address
   end
 
   it "sends the correct email to the correct address" do
-    # old address matches the 'your email has changed'
-    # new address matches 'this is your new email'
+    expect(mail.to.first).to eq(@original_email)
   end
 end
