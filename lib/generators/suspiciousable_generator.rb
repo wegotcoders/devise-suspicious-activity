606d9060fc04115d27e922e7dc8703c32228617b
Simon Knight <simon@Simons-MacBook-Air.local>
2016-02-12 13:35:21 +0000

    Refactors generator

--- lib/generators/suspiciousable_generator.rb
+++ lib/generators/suspiciousable_generator.rb
@@ -1,7 +1,7 @@
 class SuspiciousableGenerator < Rails::Generators::Base
   source_root File.expand_path("../../../app/views/suspicious_mailer", __FILE__)
 
-  def copy_initializer_file
+  def copy_change_email_notification_template
     copy_file "send_email_change_notification.html.erb", "app/views/suspiciousable/mailer/send_email_change_notification.html.erb"
   end
 end
