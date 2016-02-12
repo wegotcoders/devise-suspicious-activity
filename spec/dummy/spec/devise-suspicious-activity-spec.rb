452827f67281e429d5095c2dad7e1b923c00fa22
alexmalik <alex.malik@hotmail.co.uk>
2016-02-12 11:09:17 +0000

    Creates tests to make sure email is not on changes other than email

--- spec/dummy/spec/devise-suspicious-activity-spec.rb
+++ spec/dummy/spec/devise-suspicious-activity-spec.rb
@@ -4,29 +4,43 @@ require 'devise'
 require 'rails_helper'
 
 describe "DeviseSuspiciousActivity" do
-
   let(:original_email) { 'user@gmail.com' }
   let(:user) { User.create(email: original_email, password: 'password') }
 
-  before do
-    user.email = 'changed@gmail.com'
-    user.save!
-    @mail = ActionMailer::Base.deliveries.last
+  describe "Email change" do
+    before do
+      ActionMailer::Base.deliveries = []
+      user.email = 'changed@gmail.com'
+      user.save!
+      @mail = ActionMailer::Base.deliveries.last
+    end
+
+    it "had the correct email body" do
+      expect(@mail.body.raw_source).to include("Hi #{original_email}")
+      expect(@mail.body.raw_source).to include('We wanted to let you know that your email was changed')
+    end
+
+    it "sends one email" do
+      expect(ActionMailer::Base.deliveries.count).to eq 1
+    end
+
+    it "sends the correct email to the correct address" do
+      expect(@mail.to.first).to eq(original_email)
+    end
   end
 
-  it "sends one email" do
-    expect(ActionMailer::Base.deliveries.count).to eq 1
+  describe "Password change" do
+    before do
+      ActionMailer::Base.deliveries = []
+      user.password = "wegotcoders"
+      user.save!
+    end
+
+    it "does not send any emails" do
+      expect(ActionMailer::Base.deliveries.count).to eq 0
+    end
   end
 
-  it "had the correct email body" do
-    expect(@mail.body.raw_source).to include("Hi #{original_email}")
-    expect(@mail.body.raw_source).to include('We wanted to let you know that your email was changed')
-  end
-
-  it "sends the correct email to the correct address" do
-    expect(@mail.to.first).to eq(original_email)
-  end
-  
   # Generator for the email template
 
 end
