4d02ba4fb93359b1a57aea40acf7fd1e742f12b9
Simon Knight <simon@Simons-Air.home>
2016-02-12 18:33:48 +0000

    Changes argument name

--- app/mailers/suspicious_mailer.rb
+++ app/mailers/suspicious_mailer.rb
@@ -1,11 +1,11 @@
 class SuspiciousMailer < ActionMailer::Base
 
-  def send_email_change_notification(klass)
-    mail to: get_original_email(klass), from: devise_admin_address, subject: "Your email has changed"
+  def send_email_change_notification(obj)
+    mail to: get_original_email(obj), from: devise_admin_address, subject: "Your email has changed"
   end
 
-  def get_original_email(klass)
-    @email = klass.class.find(klass.id).email
+  def get_original_email(obj)
+    @email = obj.class.find(obj.id).email
   end
 
   def devise_admin_address
